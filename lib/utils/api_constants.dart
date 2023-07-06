b44b04b945304b1dc400a18153c82c4afefe5047
samybej <samy.bejaoui@esprit.tn>
2023-07-06 09:27:19 +0200

    added new drinks uri

--- lib/utils/api_constants.dart
+++ lib/utils/api_constants.dart
@@ -2,10 +2,10 @@ class AppConstants {
   static const String APP_NAME = "DBFood";
   static const int APP_VERSION = 1;
 
-  static const String BASE_URL = "http://mvs.bslmeiyu.com";
+  static const String BASE_URL = "http://10.0.2.2:8000";
   static const String POPULAR_PRODUCT_URI = "/api/v1/products/popular";
   static const String RECOMMANDED_PRODUCT_URI = "/api/v1/products/recommended";
-
+  static const String HEALTHY_DRINKS_URI = "/api/v1/products/drinks";
   //img
   static const String UPLOAD_IMG_URL = "/uploads/";
 
